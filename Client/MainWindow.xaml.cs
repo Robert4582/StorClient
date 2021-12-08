c9c5916045545f27e377f6e952bc466cbe049d9e
Robert4582 <43041342+Robert4582@users.noreply.github.com>
2021-12-08 15:04:52 +0100

    Test

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -24,5 +24,14 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
     }
 }
